383903877917e35e59ca872feeb3ebe6edcf7d33
Nikola Varicak <nikolavaricak01@gmail.com>
2024-10-06 21:23:44 -0400

    added all changes at once including package.json work and app src

---
+++ build/precache-manifest.4807706ca966456152448ffe3dd737f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "556fbec35c376d8f1375c64d8873879b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bc950f604c2191a2ac2",
+    "url": "/static/css/main.30d17bd7.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "7bc950f604c2191a2ac2",
+    "url": "/static/js/main.c23af39d.chunk.js"
+  },
+  {
+    "revision": "60bc7f864dddbc003152",
+    "url": "/static/js/runtime~main.71a348c3.js"
+  }
+]);
