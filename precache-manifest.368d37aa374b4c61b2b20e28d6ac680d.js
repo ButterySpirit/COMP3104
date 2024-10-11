2e0fc12af7cde98b40ef96a67f65ce72abbf1845
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2024-10-11 02:06:10 +0000

    Deploy ButterySpirit/COMP3104 to github.com/ButterySpirit/COMP3104.git:gh-pages

---
+++ precache-manifest.368d37aa374b4c61b2b20e28d6ac680d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db136491eb8926b4b25d4397cc0c67f",
+    "url": "/COMP3104/index.html"
+  },
+  {
+    "revision": "68e1852496ed8a1bf64e",
+    "url": "/COMP3104/static/css/main.30d17bd7.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/COMP3104/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/COMP3104/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "68e1852496ed8a1bf64e",
+    "url": "/COMP3104/static/js/main.4e8d1f90.chunk.js"
+  },
+  {
+    "revision": "53cf0e19f6b9d2d03a17",
+    "url": "/COMP3104/static/js/runtime~main.859d0bcd.js"
+  }
+]);
